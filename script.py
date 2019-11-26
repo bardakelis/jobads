b26e4792577ad843e3358951380a532e5f089860
V. P <vit@lijus.lt>
2019-11-26 18:10:15 +0200

    added offer start/stop dates

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import bs4, requests
+import requests, bs4
 
 exampleFile = open('/home/fogelis/proj/cvish/joblists.html')
 exampleSoup = bs4.BeautifulSoup(exampleFile.read())
@@ -17,7 +17,8 @@ for x in range(len(offer)):
     job_ad_position_name = brief_offer.find('a').text   
     company_info = brief_offer.select('ul.cvo_module_offer_meta a')   
     salary_range = brief_offer.find('span').text    
-    job_ad_url = brief_offer.find('a').get('href')
+    job_ad_href = brief_offer.find('a').get('href')
+    job_ad_url = 'https:'+job_ad_href
     
     print(' ')
     print('-------Job offer as follows:--------')
@@ -25,20 +26,34 @@ for x in range(len(offer)):
     print('Company name:', company_info[0].text)    
     print('Company location:', company_info[1].text)
     print('Salary range:', salary_range)
-    print('Job URL: https:'+job_ad_url)
+    print('Job URL:', job_ad_url)
+    
 
     print('-------End of job offer --------')
     print(' ')
     #date_submitted = brief_offer.find('span').get('content')
     #date_submitted = brief_offer.find_all('span', attrs{'itemprop':'datePosted'})
     
-    if x == len(offer) - 1:
-        print(offer[x])
+
     
+    if x == len(offer) - 1:
+        #print(offer[x])
+        # Fetch job add full description from the full job ad URL:
+        url = job_ad_url
+        look_like_a_browser = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'}
+        job_ad_page_content = requests.get(url, headers=look_like_a_browser)
+        #print(job_ad_page.text)
+        job_ad_html = bs4.BeautifulSoup(job_ad_page_content.text, 'html.parser')
+        job_ad_details = job_ad_html.select('div#page-main-content')
+        print(job_ad_details)
+        offer_valid_dates = job_ad_html.select('span.application-date')
+
+        print(offer_valid_dates[0].text)
+        print(offer_valid_dates[1].text)
  
     
-#print(type(exampleFile))
-#print(type(offer[0]))
+print(type(job_ad_html))
+print(type(job_ad_details))
 
 
 
